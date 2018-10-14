9dbf4b827725f1ff5476223a6aa9f5d204d11824
SmellydogCoding <SmellydogCoding@gmail.com>
2018-10-13 22:00:56 -0400

    sewage service

--- src/router.js
+++ src/router.js
@@ -1,17 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: () => import('./views/Home.vue'),
+      meta: { title: 'Nemacolin Home' }
+    },
+    {
+      path: '/sewage',
+      name: 'sewage',
+      component: () => import('./views/Sewage.vue'),
+      meta: { title: 'Sewage Service' }
     }
   ]
 })
---
+++ src/views/Sewage.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  v-layout(row wrap)
+    v-flex(xs12 md6)
+      v-card.mr-2.v-card--modifications
+      v-card-title(primary-title)
+        h3.headline.mx-auto.primary--text Sewage Service
+      p.px-4.mt-4 Sewage service for Nemacolin is provided by a private sewage treatment plant.&nbsp;&nbsp;Nemacolin Inc employs a Certified Plant Operator to run and maintain the plant as required by Pennsylvania Law.
+      p.px-4 Nemacolin Inc maintains the main sewage lines and manholes for the sewage system.
+      p.title.px-4 Sewage lines that connect individual homes to the main sewage line are the responsibility of the homeowner/property owner.
+    v-flex(xs12 md6)
+      img(:src="src" :alt="alt" )
+      p replace this pic with a pic of our sewage plant
+</template>
+
+<script>
+import img from '../assets/sewage plant.jpg'
+export default {
+  data () {
+    return {
+      src: img, alt: 'Sewage Plant'
+    }
+  }
+}
+</script>
