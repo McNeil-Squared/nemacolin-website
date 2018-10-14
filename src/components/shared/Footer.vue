314ef136203ffd420b9c493bf96e645f4cf38d8d
SmellydogCoding <SmellydogCoding@gmail.com>
2018-10-13 21:40:06 -0400

    add phone link to footer

--- src/components/shared/Footer.vue
+++ src/components/shared/Footer.vue
@@ -1,12 +1,17 @@
 <template lang="pug">
   v-footer(app color='primary' height='48')
     p &copy;2018 Nemacolin Inc
+    v-spacer
+    div.mr-4
+      v-btn.white--text(flat href="tel:724-320-2750")
+        v-icon fas fa-phone
+        span.white--text.ml-3 724-320-2750
 </template>
 
 <style lang="scss" scoped>
   p {
-    margin-top: 1.0rem;
-    margin-left: 0.5rem;
+    margin-top: 1.25rem;
+    margin-left: 1.5rem;
     color: #fff;
   }
 </style>
